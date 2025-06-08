02bed9da0a7360fe76e2b98021d361f2d04ff155
Quique9518 <monarcas_enrique@hotmail.com>
2025-06-08 12:52:58 -0600

    Agregar Print

--- 30_dias_python/declaracion.py
+++ 30_dias_python/declaracion.py
@@ -1,2 +1,3 @@
 print("Soy un flojo y voy a cambiar");
 print("La sintaxis de este lenguaje de programación es más sencilla")
+print("Vamos a comenzar con el día 2 de chatGPT donde ya se va  atirar codigo.")
